436973310e0b3219547d3863eddf4c26aa16886b
Parate <Laxmikant.Parate@wkfs-frc.local>
2022-07-10 16:28:12 +0530

    patient load data, update data, save data using json files

--- Python/2022-06-28/patient.py
+++ Python/2022-06-28/patient.py
@@ -1,6 +1,21 @@
-# show_main_memu
-# show_patient_menu
-# load_data
-# update_data
-# show_patient_data
-# add_patient
+import json
+
+def load_patient_data():
+    with open(r'Python\2022-06-28\data.json', 'r') as json_file_handler:
+        data = json.load(json_file_handler)
+    return data
+
+def update_data(id, name, patient_data):
+    for patient in patient_data['patient_list']:
+        if patient['patient_id'] == id:
+            patient['firstname'] = name
+    
+def save_data(patient_data):
+    with open(r'Python\2022-06-28\data.json', 'w') as json_file_handler:
+        json.dump(patient_data, json_file_handler, indent=2)
+
+if __name__ == '__main__':
+    patient_data = load_patient_data()
+
+    update_data(2, "Sita", patient_data)
+    save_data(patient_data)

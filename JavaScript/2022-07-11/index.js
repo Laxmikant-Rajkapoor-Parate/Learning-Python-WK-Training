db4ededddf7e32cab4ce6824a664193ebb919a21
Parate <Laxmikant.Parate@wkfs-frc.local>
2022-08-06 00:01:10 +0530

    JavaScript arrays & functions, Interaction of web UI elements with JS like buttons and textboxes

---
+++ JavaScript/2022-07-11/index.js
@@ -0,0 +1,13 @@
+// const onLoad = () => {
+//   alert(document.getElementById("uname").value);
+// };
+
+const oneClick = () => {
+  const one = document.getElementById("one").value;
+  document.getElementById("name").value = one;
+};
+
+const twoClick = () => {
+  const two = document.getElementById("two").value;
+  document.getElementById("name").value = two;
+};

9b2d89933cec134698f1d6b18cb4b684e91a032f
Parate <Laxmikant.Parate@wkfs-frc.local>
2022-08-05 23:26:33 +0530

    Python completed, Introduction to JavaScript

---
+++ JavaScript/2022-07-08/first.js
@@ -0,0 +1,19 @@
+// console.log("Hello World");
+
+// a = 5
+// b = 10
+// ans = a + b
+// console.log(ans)
+
+// var a = 5
+// var b = 10
+// var ans = a + b
+// console.log(ans)
+
+var person = {
+  name: "Ajay",
+  age: 45,
+};
+
+console.log(person.name);
+console.log(person["age"]);
---
+++ JavaScript/2022-07-08/slice.js
@@ -0,0 +1,12 @@
+const str = "Jaydeep";
+
+// console.log(str[0]);
+// console.log(str.indexOf("y"));
+
+var slicedStr = str.slice(0, 2);
+console.log(slicedStr);
+
+slicedStr = str.slice(0, str.length);
+console.log(slicedStr);
+
+console.log(`His name is ${str}`)

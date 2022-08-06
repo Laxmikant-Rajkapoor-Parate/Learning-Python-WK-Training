ad7d05438a7e4d7e11175765897db4b9b6db221c
Parate <Laxmikant.Parate@wkfs-frc.local>
2022-08-06 13:10:50 +0530

    Worked further on calculator, destructuring and combinations of array in JS

---
+++ JavaScript/2022-07-15/destructuring.js
@@ -0,0 +1,19 @@
+// const student = {
+//   name: "Laxmikant",
+//   gender: "Male",
+//   age: 21,
+// };
+
+// const { name } = student;
+
+// console.log(name);
+
+const arr = [1, 2, 3];
+// [a, b, c] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+const newArr = [...arr, 4];
+console.log(newArr);
